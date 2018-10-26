e61cf415599f2be19d6a68c20ee85ef5c34d9e44
namitjalota <jalotasargam@yahoo.co.in>
2018-10-26 12:56:42 +0530

    Login Page

--- app/src/main/java/com/example/sargam/studentportal/MainActivity.java
+++ app/src/main/java/com/example/sargam/studentportal/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.sargam.studentportal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button Login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Login=findViewById(R.id.login);
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
