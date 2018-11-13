48b1aab81925cf3db971f1ffb0c4344a48a41b9c
namitjalota <jalotasargam@yahoo.co.in>
2018-11-13 17:08:16 +0530

    Time Table

--- app/src/main/java/com/example/sargam/studentportal/HomeActivity.java
+++ app/src/main/java/com/example/sargam/studentportal/HomeActivity.java
@@ -1,7 +1,13 @@
 package com.example.sargam.studentportal;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +15,48 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        NavigationView navView=findViewById(R.id.nav_menu);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.profile:
+                    {
+                        Intent i=new Intent(HomeActivity.this,ProfileActivity.class);
+                        startActivity(i);
+                        break;
+                    }
+                    case R.id.search:
+                    {
+                        Intent i=new Intent(HomeActivity.this,SearchActivity.class);
+                        startActivity(i);
+                        break;
+                    }
+                    case R.id.report:
+                    {
+                        Intent i=new Intent(HomeActivity.this,ReportActivity.class);
+                        startActivity(i);
+                        break;
+                    }
+                    case R.id.reviews:
+                    {
+                        Intent i=new Intent(HomeActivity.this,ReviewActivity.class);
+                        startActivity(i);
+                        break;
+                    }
+                    case R.id.logout:
+                    {
+                        Intent i=new Intent(HomeActivity.this,MainActivity.class);
+                        startActivity(i);
+                        break;
+                    }
+                }
+                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+                drawer.closeDrawer(GravityCompat.START);
+
+                return true;
+            }
+        });
     }
 }
--- app/src/main/java/com/example/sargam/studentportal/MainActivity.java
+++ app/src/main/java/com/example/sargam/studentportal/MainActivity.java
@@ -1,18 +1,26 @@
 package com.example.sargam.studentportal;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button Login;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         Login=findViewById(R.id.login);
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -21,5 +29,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+
     }
+
 }
