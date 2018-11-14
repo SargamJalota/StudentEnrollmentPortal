edac64103bd30bc0a934e0589d861f094a787232
namitjalota <jalotasargam@yahoo.co.in>
2018-11-15 02:56:30 +0530

    Different Pages

--- app/src/main/java/com/example/sargam/studentportal/ReportActivity.java
+++ app/src/main/java/com/example/sargam/studentportal/ReportActivity.java
@@ -2,12 +2,15 @@ package com.example.sargam.studentportal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class ReportActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+
     }
 }
